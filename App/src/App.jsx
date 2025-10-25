ce235ebe4ed30ff22554be5edd5e1ed8624582f3
Mat0108 <61587759+Mat0108@users.noreply.github.com>
2025-10-25 18:34:33 +0200

    migrate to vite projet

---
+++ App/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen bg-darkBlue flex flex-col center text-[50px] text-white font-mt-bold'>
+        <p>Guillaume Barnabé</p>
+        <p>Work in progress</p>
+      </div>
+    </>
+  )
+}
+
+export default App
